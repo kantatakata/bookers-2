ee84a6919a95f4b2e798f9b18d33d985510d3435
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-09-21 10:11:04 +0000

    [add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,21 @@ class BooksController < ApplicationController
   end
 
   def index
+   @book = Book.new
   end
+  def create
+   book = book.new(book_params)
+   book.save
+   redirect_to'book/show'
+  end 
 
   def show
+  end
+  
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
