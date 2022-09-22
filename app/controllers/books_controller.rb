b1513fb8efb985f78732df383a546b6027a23388
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-09-22 15:55:10 +0000

    [add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,45 @@
 class BooksController < ApplicationController
-  def new
-  end
+   def new
+   end
 
-  def index
-   @book = Book.new
-   @books = Book.all
-   @user = current_user
-  end
+   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+   end
   
-  def create
-  @book = Book.new(book_params)
-  @book.user_id = current_user.id
+   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
    if @book.save
       redirect_to book_path(@book.id), notice: 'Book was successfully created.'
    else
-    @books = Book.all
-    render :index
+     @books = Book.all
+     render :index
    end
-  end
+   end
   
-  def show
-   @book = Book.find(params[:id])
-   @user = current_user
-   
-  end
+   def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @books = Book.new
+   end
   
+   def edit
+    @book = Book.find(params[:id])
+   end
   
+   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+
+      render :edit
+    end
+   end
   private
   # ストロングパラメータ
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/homes/about" => "homes#about", as: "about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
